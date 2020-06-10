611d4ba23ee0551fc7ce1af3110ce2125d19f1e5
Keith <keith@keithkurson.net>
2020-06-10 11:13:24 -0700

    try using themeUI

---
+++ src/theme.js
@@ -0,0 +1,24 @@
+export default {
+  fonts: {
+    body: '"Benton Sans", system-ui, sans-serif',
+    heading: '"Benton Sans", "Avenir Next", sans-serif',
+    monospace: 'Menlo, monospace',
+  },
+  colors: {
+    text: '#000',
+    background: '#fff',
+    primary: '#33e',
+  },
+  text: {
+    caps: {
+      textTransform: 'uppercase',
+      letterSpacing: '0.2em',
+    },
+    heading: {
+      fontFamily: 'heading',
+      fontWeight: '700',
+      fontSize: 8,
+      lineHeight: 'heading',
+    },
+  }
+}
